d869765e8bce8e5d1ca8f17b561186bcd2c4c1fd
OrGaNiK777 <OrGaNiK777@yandex.ru>
2023-07-16 16:39:53 +0500

    16.07.2023 dom v15

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,13 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 // const helmet = require('helmet');
 
-const cors = require('./middlewares/cors');
+//const cors = require('./middlewares/cors');
 const routes = require('./routes/index');
 const customErrorsHandler = require('./middlewares/customErrorsHandler');
 const { PORT, host } = require('./utils/consctants');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
-
-//app.use(express.static('../frontend/build'));
 
 mongoose
   .connect(`mongodb://${host}:27017/MestoDB`, {
@@ -27,7 +25,7 @@ app.use(express.json());
 
 app.use(requestLogger);
 
-app.use(cors);
+//app.use(cors);
 
 // app.use(helmet());
 
